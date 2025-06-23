db5062fb4cc9797c6abdc06ea3e9f352688ac42a
Myles Slack <binglaplap@gmail.com>
2025-06-23 14:24:51 -0400

    Initial Commit

---
+++ MoleculeAR Pro/MoleculeAR_ProApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoleculeAR_ProApp.swift
+//  MoleculeAR Pro
+//
+//  Created by Myles Slack on 2025.06.23.
+//
+
+import SwiftUI
+
+@main
+struct MoleculeAR_ProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoleculeAR ProTests/MoleculeAR_ProTests.swift
@@ -0,0 +1,16 @@
+//
+//  MoleculeAR_ProTests.swift
+//  MoleculeAR ProTests
+//
+//  Created by Myles Slack on 2025.06.23.
+//
+
+import Testing
+
+struct MoleculeAR_ProTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
